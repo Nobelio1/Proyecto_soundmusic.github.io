db10e5ff107623aa0ca0e17a2cdab0fe5b8b47d9
gino-Nobelio <ginoalejandro2304@gmail.com>
2022-11-14 00:00:27 -0500

    version 6

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    //Header
+    $(window).on('scroll',function(){
+        let posicion = $('#pr').offset();
+        if ($(window).scrollTop() >= posicion.top){
+            $('header').removeClass('transparente');
+        }else{
+            $('header').addClass('transparente');
+        }
+    })
+    //Smooth scroll
+    $('a').on('click',function(){
+        if (this.hash !== ''){
+            let hash = this.hash;
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            },800)
+        }
+    })
+})
